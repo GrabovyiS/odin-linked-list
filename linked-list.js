38664bbd1d9ca6a3c50e81fd5c8833cdbe8336f0
GrabovyiS <graboviys@yandex.ru>
2024-07-20 02:32:37 +0300

    feat: linked list with insertAt() and removeAt()

---
+++ linked-list.js
@@ -0,0 +1,200 @@
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList {
+  head = null;
+  size = 0;
+  tail = null;
+
+  append(value) {
+    const newNode = new Node(value);
+
+    // If it is the first element of the list
+    if (!this.head && !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
+    }
+
+    this.size++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    // If it is the first element of the list
+    if (!this.head && !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+
+    this.size++;
+  }
+
+  at(index) {
+    if (index > this.size - 1) {
+      return false;
+    }
+
+    let i = 0;
+    let currentElement = this.head;
+    while (i !== index) {
+      currentElement = currentElement.nextNode;
+      i++;
+    }
+
+    return currentElement;
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return false;
+    }
+
+    let secondLastElement = this.head;
+
+    while (secondLastElement.nextNode !== this.tail) {
+      secondLastElement = secondLastElement.nextNode;
+    }
+
+    this.tail = secondLastElement;
+    secondLastElement.nextNode = null;
+
+    this.size--;
+  }
+
+  contains(value) {
+    let currentElement = this.head;
+    while (currentElement) {
+      if (currentElement.value === value) {
+        return true;
+      }
+      currentElement = currentElement.nextNode;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let i = 0;
+    let currentElement = this.head;
+    while (currentElement) {
+      if (currentElement.value === value) {
+        return i;
+      }
+      currentElement = currentElement.nextNode;
+      i++;
+    }
+
+    return false;
+  }
+
+  toString() {
+    let string = '';
+
+    let currentElement = this.head;
+    while (currentElement) {
+      string = string + `( ${currentElement.value} ) -> `;
+      currentElement = currentElement.nextNode;
+    }
+
+    // When we have reached the tail
+    string = string + 'null';
+    return string;
+  }
+
+  insertAt(value, index) {
+    // First/last as edge cases to not manage tail/head later
+    if (index >= this.size - 1) {
+      // Could return false when index > this.size - 1 but
+      this.append(value);
+      return;
+    }
+
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+
+    let i = 0;
+    let currentElement = this.head;
+    let previousElement;
+
+    while (i !== index) {
+      previousElement = currentElement;
+      currentElement = currentElement.nextNode;
+      i++;
+    }
+
+    // When we arrive at the index, currentElement is now stored at that index
+    const newNode = new Node(value);
+    previousElement.nextNode = newNode;
+    newNode.nextNode = currentElement;
+
+    this.size++;
+  }
+
+  removeAt(index) {
+    // First/last as edge cases to not manage tail/head later
+    if (index === 0) {
+      this.head = this.head.nextNode;
+    }
+
+    if (index === this.size - 1) {
+      this.pop();
+    }
+
+    if (index > this.size - 1) {
+      return false;
+    }
+
+    let i = 0;
+    let currentElement = this.head;
+    let previousElement;
+
+    while (i !== index) {
+      previousElement = currentElement;
+      currentElement = currentElement.nextNode;
+      i++;
+    }
+
+    // When we arrive at the index, currentElement is now stored at that index
+    previousElement.nextNode = currentElement.nextNode;
+
+    this.size--;
+  }
+}
+
+const list = new LinkedList();
+
+list.append('dog');
+list.append('cat');
+list.prepend('crow');
+list.prepend('parrot');
+list.pop();
+
+console.log('Head:', list.head);
+console.log('Tail:', list.tail);
+console.log('Size:', list.size);
+
+console.log('at(1):', list.at(1));
+
+console.log("contains('dog'):", list.contains('dog'));
+console.log("find('dog')", list.find('dog'));
+
+console.log('toString()', list.toString());
+
+list.insertAt('bear', 1);
+console.log("After insertAt('bear', 1):", list.toString());
+
+list.removeAt(1);
+console.log('After removeAt(1):', list.toString());
